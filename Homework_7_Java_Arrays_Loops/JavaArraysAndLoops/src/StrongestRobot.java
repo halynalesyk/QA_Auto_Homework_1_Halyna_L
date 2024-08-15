fb3ab56447c7f3e27bcb028b93bd0ed658c77ece
Halyna Lesyk <halya.by.xpd@gmail.com>
2024-08-15 14:36:01 +0200

    added new files

--- Homework_7_Java_Arrays_Loops/JavaArraysAndLoops/src/StrongestRobot.java
+++ Homework_7_Java_Arrays_Loops/JavaArraysAndLoops/src/StrongestRobot.java
@@ -1,2 +1,15 @@
 public class StrongestRobot {
+    public static void main(String[] args) {
+        int[] weights = {300, 450, 200, 500, 350};
+        int maxWeight = weights[0];
+        int strongestRobot = 0;
+        for (int i = 1; i < weights.length; i++) {
+            if (weights[i] > maxWeight) {
+                maxWeight = weights[i];
+                strongestRobot = i;
+            }
+        }
+        System.out.println("Strongest Robot is: " + (strongestRobot + 1));
+        System.out.println("Weight is: " + maxWeight);
+    }
 }
